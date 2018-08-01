34b316527c4ec2a2dad1d219d178a3cb99450bc7
TessACraig89 <tesscraig.webdev@gmail.com>
2018-07-31 20:59:51 -0500

    sets a ref property on the text field, which is a callback function to save a reference to that text field in our Account object, so the data in the field can be accessed later

--- src/Account.js
+++ src/Account.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 
 export default class Account extends Component {
   render() {
+    // set a ref property on text field, this is a callback function to save a reference to the text field in out Account object. This is how we can access the data in the field later 
     return (
       <div className="account">
         <h2>{this.props.name}</h2>
         <div className="balance">$0</div>
-        <input type="text" placeholder="enter an amount" />
+        <input type="text" placeholder="enter an amount" ref={(input) => this.inputBox = input}/>
         <input type="button" value="Deposit" />
         <input type="button" value="Withdraw" />
       </div>
